2aae8cd455b2404d95238053f1f5beaed1eb8a7b
KatiaRassadina <114443286+KatiaRassadina@users.noreply.github.com>
2022-10-23 12:24:10 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает трехзначное число и на выходе показывает вторую цифру этого число 
+int N = 0;
+Console.WriteLine("Input a three-digit number");
+N = Convert.ToInt32(Console.ReadLine());
+if (N >= 100 && N <= 999)
+{
+    int DivideByTen = N / 10;
+    int LastDigit = DivideByTen % 10;
+    Console.WriteLine("The second digit is " +LastDigit);
+}
+else
+{
+    Console.WriteLine("Not suitable");
+}
